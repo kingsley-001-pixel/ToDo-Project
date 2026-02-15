68fa6cd1a48dfef696888e8c35fbe4cbc810642b
kingsley-001-pixel <240056576+kingsley-001-pixel@users.noreply.github.com>
2026-02-15 15:37:20 +0100

    Modified html file and added comments.

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const taskInput = document.getElementById('taskInput')
 const addTaskBtn = document.getElementById('addTaskBtn')
 const taskContainer = document.getElementById('taskContainer')
 
+// ADD TASK FUNCTION STATEMENT
 const addTask = () => {
+    // GETS VALUE OF INPUT FIELD
     taskInputValue = taskInput.value
+
+    // EXITS FUNCTION IF INPUT IS EMPTY
     if (!taskInputValue) {
         return;
     }
+
+    // IF THERE ARE NO TASKS, ADD HEADING
     if(taskContainer.children.length === 0) {
         const heading = document.createElement('h1')
         heading.id = 'taskHeading'
@@ -14,6 +20,8 @@ const addTask = () => {
         heading.className = 'text-2xl text-center underline'
         taskContainer.appendChild(heading)
     }
+    
+    // CREATES LIST ITEM AND DELETE BUTTON, APPENDS TO TASK CONTAINER, AND RESETS INPUT FIELD
     const listItem = document.createElement('li')
     const delBtn = document.createElement('button')
     delBtn.className = 'delBtn';
@@ -24,20 +32,22 @@ const addTask = () => {
     document.getElementById('taskInput').value = ""
 }
 
+// EVENT LISTENERS
 document.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         addTask()
     }
 })
-
 addTaskBtn.addEventListener('click', addTask)
 
+// DELETE TASK FUNCTIONALITY USING EVENT DELEGATION
 taskContainer.addEventListener('click', (e) => {
     const btn = e.target.closest('button')
     if(!btn) {
         return;
     }
 
+    // REMOVES THE TASK AND HEADING IF THERE ARE NO MORE TASKS
     const li = btn.closest('li');
     li.remove();
     if(taskContainer.children.length === 1) {
