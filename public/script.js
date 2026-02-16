1d27f3e262e9e7cb80baf04595808d91c1669ad9
kingsley-001-pixel <240056576+kingsley-001-pixel@users.noreply.github.com>
2026-02-16 16:41:58 +0100

    Added deletion of task function in local storage

--- public/script.js
+++ public/script.js
@@ -2,6 +2,30 @@ const taskInput = document.getElementById('taskInput')
 const addTaskBtn = document.getElementById('addTaskBtn')
 const taskContainer = document.getElementById('taskContainer')
 
+document.addEventListener('DOMContentLoaded', () => {
+    const storedTasksArr = []
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    if (!storedTasks) {
+        return;
+    }
+    storedTasksArr.push(...storedTasks);
+    storedTasksArr.forEach(task => {
+        const listItem = document.createElement('li')
+        const delBtn = document.createElement('button')
+        delBtn.className = 'delBtn';
+        listItem.textContent = task.task
+        delBtn.textContent = '❌'
+        if (!task.task) {
+            return;
+        } else {
+            listItem.appendChild(delBtn);
+            taskContainer.appendChild(listItem);
+        }
+    })
+})
+
+
 // ADD TASK FUNCTION STATEMENT
 const addTask = () => {
     // GETS VALUE OF INPUT FIELD
@@ -21,23 +45,28 @@ const addTask = () => {
         taskContainer.appendChild(heading)
     }
 
-    const taskArray = [];
+    const taskArray = JSON.parse(localStorage.getItem('tasks')) || [];
 
     // CREATES LIST ITEM AND DELETE BUTTON, APPENDS TO TASK CONTAINER, AND RESETS INPUT FIELD
     const listItem = document.createElement('li')
     const delBtn = document.createElement('button')
+    const id = document.createAttribute('data-id')
+    id.value = Date.now()
+    delBtn.setAttributeNode(id)
     delBtn.className = 'delBtn';
     listItem.textContent = taskInputValue
     delBtn.textContent = '❌'
-    const listItemObj = {
-        id: Date.now(),
+
+    const newTask = {
+        id: id.value,
         task: taskInputValue,
         completed: false
     }
+
+    taskArray.push(newTask)
     
     // STORES TASKS IN LOCAL STORAGE
-    localStorage.setItem('tasks', JSON.stringify(listItemObj))
-    taskArray.push(listItemObj)
+    localStorage.setItem('tasks', JSON.stringify(taskArray))
 
     // APPENDING ITEMS TO DOM
     listItem.appendChild(delBtn);
@@ -62,6 +91,10 @@ taskContainer.addEventListener('click', (e) => {
 
     // REMOVES THE TASK AND HEADING IF THERE ARE NO MORE TASKS
     const li = btn.closest('li');
+    const taskId = btn.getAttribute('data-id');
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const updatedTasks = storedTasks.filter(task => task.id !== taskId);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
     li.remove();
     if(taskContainer.children.length === 1) {
         const heading = document.getElementById('taskHeading')
